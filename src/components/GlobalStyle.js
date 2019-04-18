1672fd338ffba9a51c6f7475c2585ddd2828e5f0
Kasia Zadurska <kzadurska@gmail.com>
2019-04-18 17:01:06 +0200

    Modify blockqoute styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -23,6 +23,15 @@ const GlobalStyle = createGlobalStyle`
       text-decoration: none;
     }
   }
+
+  blockquote {
+    margin: 0;
+    border-left: 5px solid rgba(0, 88, 130, .3);
+    padding: 0 1em;
+
+    font-style: italic;
+    font-weight: 300;
+  }
 `
 
 export default GlobalStyle
